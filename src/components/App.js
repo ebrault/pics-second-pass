7a1f514afdfb5f780bb41e560fd1b4efe58d5955
ebrault <emmet.brault@gmail.com>
2019-04-10 16:46:36 -0400

    Added Axios, Added API Request
    Installed axios library and added a get request to our third party API
    to retrieve information that matches images for our queries
      - Axios has a built in get method that accepts a url and a request
        body to handle params and authorization
      - This function is going to be implemented in our submission function
        - Referred to API documentation in order to make a well formed API
          request
          - Base URL gets passed in as a string to the first argument of the
            axios.get function
          - Headers object added to request body as an object with k/v pairs
            - In this case the only header we are adding to the request body
              is our auth token generated by the API
          - Queries to the API can be made in multiple ways, but we are
            going to pass our query in as a parameter to the request body
            using the params object and a k/v pair of query: term where
            the term passed into the query value is the submitted term
            passed as an argument to the submission function
      - Verified that our API request works by opening our network dev tool
        adding a search to our form, submitting it, and looking at the
        response from the server

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 import ImageList from './ImageList'
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 37ad812b117ddfc782666b8f04441f1acdd0ea7c62597c1defbcf7f13341b014'
+      }
+    })
   }
   render() {
     return (
