cdac7ab8b0358ba289181f96501af3e8bcaada02
ebrault <emmet.brault@gmail.com>
2019-04-10 15:40:26 -0400

    Refactored SearchBar To Controlled Component
    Refactored SearchBar from uncontrolled component to controlled component
    - Because SearchBar is a class based component and will be managing
      state, we are going to initialize state with a key/value pair to track
      what is typed into our search bar in state
      - The key is going to be titled term and it is going to be initialized
        as an empty string
      - Remove our onInputChange callback and replace it with an arrow fn
        passed to onChange
        - Arrow function is going to be called with event passed as an
          argument, and rather than logging the changes made to the form
          we are going to call setState and assign term to the target.value
          property of event
        - This is going to set the state of the form to whatever is typed
          into it
        - A value prop is passed to our form element with the value
          this.state.term
    - This refactor is refactoring our uncontrolled component to a
      controlled one:
      - The user types input into the search bar
      - The event handler callback gets invoked
      - setState gets called and the value of term gets reassigned to the
        input
      - The component rerenders with the input
      - The form is told what it's value is from state passed to the
        value prop or by manually controlling the value prop
        - I.e. if we put 'Enter text here' into the value prop, it would
          render when the component is loaded
    
    - With an uncontrolled element, the only way we can know the value of
      an input at a given moment in time is retrieving it from the DOM and
      not in our React component
    - With a controlled component, we are able to know the value of an input
      at a given moment because it is *controlled* in our React component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,7 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value)
-  }
-  onInputClick() {
-    console.log('Input was clicked')
-  }
+  state = { term: '' }
 
   render() {
     return (
@@ -14,7 +9,7 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onClick={this.onInputClick} onChange={this.onInputChange}/>
+            <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value })}/>
           </div>
         </form>
       </div>
