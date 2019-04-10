f6a6a4251beb8d33b4a1de6c7807723331554f75
ebrault <emmet.brault@gmail.com>
2019-04-10 17:07:05 -0400

    Refactored App To Begin Rendering
    We currently have no way of manipulating the responses that we receive
    from the API, so we need a way to store them
      - State is ideal for this
        - Initialize state to a k/v pair of images/empty array
          - We are using array since this is the type that results is
            already formed as
        - Rather than logging results, we are going to insert the results
          into state by calling setState and assigning it to results
      - We are not quite ready to render our results as images yet, but
        we can get a count of the images that the API responds with by
        passing a this.state.images.length to a variable in our return div
        - This is another reason we are using an empty array as our type for
          state, because we can get the length of an empty array (0) and we
          cannot get the length of "null"
      - React initially throws an error because it cannot read the context
        of this.state from our onSearchSubmit function which is currently
        unbound
        - We can either bind this or convert it to an arrow function
          - Converting async/await functions to arrow functions requires
            us to refactor the function to an arrow function like normal but
            to shift the async keyword to before the arguments
      - We are now able to submit a search and see the number of results
        that the API responds with

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,21 @@ import SearchBar from './SearchBar'
 import ImageList from './ImageList'
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] }
+  onSearchSubmit = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID 37ad812b117ddfc782666b8f04441f1acdd0ea7c62597c1defbcf7f13341b014'
       }
     })
-    console.log(response.data.results)
+    this.setState({ images: response.data.results })
   }
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        {this.state.images.length}
         <ImageList />
       </div>
     )
